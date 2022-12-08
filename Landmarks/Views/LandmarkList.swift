6058333076739a1987227fd60052cfb1f4f898ff
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-12-08 23:27:20 +0300

    Create a list of attractions in LandmarkList

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by NikitaChekan on 08.12.2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
