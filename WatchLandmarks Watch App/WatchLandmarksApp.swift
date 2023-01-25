eeab195a97bf0eee5f131f411bb73e611afcd705
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-01-25 15:38:29 +0300

    add a watchOS Target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by jopootrivatel on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
